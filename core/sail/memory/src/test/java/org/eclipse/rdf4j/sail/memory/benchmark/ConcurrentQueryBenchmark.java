5582715a009c83b62bbb4601511d30f2ad3860b6
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-12-11 17:30:32 -0500

    Use a higher max heap size.

--- core/sail/memory/src/test/java/org/eclipse/rdf4j/sail/memory/benchmark/ConcurrentQueryBenchmark.java
+++ core/sail/memory/src/test/java/org/eclipse/rdf4j/sail/memory/benchmark/ConcurrentQueryBenchmark.java
@@ -43,7 +43,7 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @State(Scope.Benchmark)
 @Warmup(iterations = 5)
 @BenchmarkMode({ Mode.AverageTime })
-@Fork(value = 1, jvmArgs = { "-Xms1G", "-Xmx1G", })
+@Fork(value = 1, jvmArgs = { "-Xms1G", "-Xmx50G", })
 @Measurement(iterations = 5)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class ConcurrentQueryBenchmark extends BaseConcurrentBenchmark {
