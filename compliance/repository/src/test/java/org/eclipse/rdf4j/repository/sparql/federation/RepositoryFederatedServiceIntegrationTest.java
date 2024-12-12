5db29ea880c0dc67b1969afd29ddd847e3ea333a
Benjamin Prud'homme <bprudhomme@gradcenter.cuny.edu>
2024-12-12 09:19:44 -0500

    Remove unnecessary flags

--- compliance/repository/src/test/java/org/eclipse/rdf4j/repository/sparql/federation/RepositoryFederatedServiceIntegrationTest.java
+++ compliance/repository/src/test/java/org/eclipse/rdf4j/repository/sparql/federation/RepositoryFederatedServiceIntegrationTest.java
@@ -67,7 +67,7 @@ import com.google.common.collect.Lists;
 @State(Scope.Benchmark)
 @Warmup(iterations = 5)
 @BenchmarkMode({ Mode.AverageTime })
-@Fork(value = 1, jvmArgs = { "-Xms1G", "-Xmx1G", "-XX:+UseG1GC" })
+@Fork(value = 1, jvmArgs = { "-Xms1G", "-Xmx50G", })
 @Measurement(iterations = 5)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class RepositoryFederatedServiceIntegrationTest {
--- core/sail/lucene/src/test/java/org/eclipse/rdf4j/sail/lucene/impl/AbstractGenericLuceneTest.java
+++ core/sail/lucene/src/test/java/org/eclipse/rdf4j/sail/lucene/impl/AbstractGenericLuceneTest.java
@@ -91,7 +91,7 @@ import org.slf4j.LoggerFactory;
 @State(Scope.Benchmark)
 @Warmup(iterations = 5)
 @BenchmarkMode({ Mode.AverageTime })
-@Fork(value = 1, jvmArgs = { "-Xms1G", "-Xmx1G", "-XX:+UseG1GC" })
+@Fork(value = 1, jvmArgs = { "-Xms1G", "-Xmx50G", })
 @Measurement(iterations = 5)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 public abstract class AbstractGenericLuceneTest {
